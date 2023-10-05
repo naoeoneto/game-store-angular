fc0de22de71d950d55d73019092d96de72ca8c46
naoeoneto <abdpneto@gmail.com>
2023-10-05 11:52:00 -0300

    final commit

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-card-pricing',
-  templateUrl: './card-pricing.component.html',
-  styleUrls: ['./card-pricing.component.css']
+	selector: 'app-card-pricing',
+	templateUrl: './card-pricing.component.html',
+	styleUrls: ['./card-pricing.component.css'],
 })
 export class CardPricingComponent implements OnInit {
+	@Input()
+	gameType: string = 'Digital PS4';
 
-  constructor() { }
+	@Input()
+	gameValue: string = 'R$149,99';
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+	selector: 'app-card',
+	templateUrl: './card.component.html',
+	styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+	@Input()
+	gameCover: string = '';
 
-  constructor() { }
+	@Input()
+	gameLabel: string = '';
 
-  ngOnInit(): void {
-  }
+	@Input()
+	gameType: string = 'Digital PS4';
 
+	@Input()
+	gameValue: string = 'R$149,99';
+	constructor() {}
+
+	ngOnInit(): void {}
 }
